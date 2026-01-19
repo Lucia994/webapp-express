b824e6f7a4f2f6e920922953fc4c33ab26c1528b
Lucia994 <l.summa94@gmail.com>
2026-01-19 17:55:34 +0100

    Define initial project settings

---
+++ database/dbConnection.js
@@ -0,0 +1,14 @@
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE , 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
+
+connection.connection((err)=>{
+ if(err) throw err;
+ console.log(`Connected to MySQL!`)
+})
+export default connection;
